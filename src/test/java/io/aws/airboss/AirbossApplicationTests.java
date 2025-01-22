332a2fd49d856c1adb8b9b6c45430f5f7afec757
uxoa <palomita@gmail.com>
2025-01-22 21:05:30 +0100

    first commit

---
+++ src/test/java/io/aws/airboss/AirbossApplicationTests.java
@@ -0,0 +1,13 @@
+package io.aws.airboss;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AirbossApplicationTests {
+    
+    @Test
+    void contextLoads() {
+    }
+    
+}
